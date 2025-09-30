e3848d0baeba0cd2a05ccba33bb8a2487e771f96
krub-dev <kikorubioillan@gmail.com>
2025-09-30 12:27:43 +0200

    archivo base con subjects y readme explicativo. Time to practice

---
+++ Level-1/ft_scanf/ft_scanf.c
@@ -0,0 +1,8 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+int ft_scanf(const char *format, ...)
+{
+    // Implementación aquí.
+    return 0;
+}
---
+++ Level-2/rip/rip.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    // Implementación aquí: eliminar el mínimo número de paréntesis para balancear
+    return 0;
+}
